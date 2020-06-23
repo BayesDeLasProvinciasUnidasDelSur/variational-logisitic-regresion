7dbfac2f65cc6a462e56bf841d97475f9a197dea
Gustavo Landfried <gustavolandfried@gmail.com>
2020-06-23 12:02:32 -0300

    Add url to analytical solution

--- src/__init__.py
+++ src/__init__.py
@@ -6,8 +6,7 @@ def sigmoid(z):
 
 def _lambda(xi):#xi=xis; xi=Phi.dot(w)
     """
-    El t\'ermino (xi==0) se agrega para definir una funci\'on partida
-    Cunado xi == 0 quiero que devuelva 0. False*algo == 0
+    Cunado xi == 0 quiero que devuelva 0
     """
     div0 = np.divide(1, (2*xi), out=np.zeros_like(xi), where=xi!=0)
     return (-div0 * (sigmoid(xi)-(1/2)))
@@ -23,16 +22,19 @@ def figure_10_12_b():
     """
     Replica la figura 10.12 right de bishop
     Gaussian lower bound for the logistic
+    TODO:
+        [] Agregar par\'ametro bool para imprimir figura
     """
     xs = np.arange(-6,6,0.1)
     plt.plot(xs,sigmoid(xs))
     x=2.5
     plt.scatter(x,sigmoid(x))
     plt.plot(xs,logistic_lower_bound(xs,x))
+    plt.show()
 
 def polynomial_basis_function(x, degree=1):
     return x ** degree
-    
+
 def likelihood(ts,w,Phi):
     """
     result == (sigmoid(a)**ts) * ((1-sigmoid(a))**(1-ts) )
@@ -55,14 +57,31 @@ def posterior(ts,Phi,mu0=None,S0=None,alpha=1e8):
         xis = EM procedure
         - Bayesian parameter estimation via variational methods
         - Bishop 10.6.2
+        see:
+            https://github.com/zhengqigao/PRML-Solution-Manual/blob/master/Solution%20Manual%20For%20PRML.pdf
     """
-    
+
     S0_inv = np.linalg.inv(S0)
     SN_inv = S0_inv + 2*Phi.T.dot(_lambda(xis)*Phi)
     SN = np.linalg.inv(SN_inv)
     muN = SN.dot(S0_inv.dot(mu0) + (ts - 1/2).T.dot(Phi).T )
     return muN, SN    
-    
+
+
+
+
+"""
+El siguiente codigo debe ser migrado a test.py
+"""
+
+"""
+El likelihood variacional y el likelihood son exactamente iguales
+"""
+np.sum(np.log(likelihood(ts,w,Phi)))
+np.sum(np.log(variational_likelihood(ts,w,Phi)))
+
+
+
 edades_grilla = np.arange(0,100,1).reshape((100,1))
 Phi_grilla = polynomial_basis_function(edades_grilla,np.array(range(2)))
 slope = 0.1
@@ -79,8 +98,8 @@ z = Phi_grilla.dot(w)
 
 
 plt.plot(Phi_grilla.dot(w))
-
-
+plt.show()
+plt.close()
 
 N=1000
 edades =np.random.randint(1,100,N).reshape((N,1))
@@ -88,15 +107,11 @@ Phi = polynomial_basis_function(edades,np.array(range(2)))
 ts = np.random.binomial(1,sigmoid(Phi.dot(w)))
 plt.scatter(edades,ts)
 
+
 muN, SN = posterior(ts,Phi)
 plt.plot(edades_grilla,sigmoid(Phi_grilla.dot(muN)))
+plt.show()
 
 
-
-"""
-El likelihood variacional y el likelihood son exactamente iguales
-"""
-np.sum(np.log(likelihood(ts,w,Phi)))
-np.sum(np.log(variational_likelihood(ts,w,Phi)))
 
 
